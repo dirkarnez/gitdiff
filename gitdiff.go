b1e5634561de557025b570312da93bc52c5209b8
dirkarnez <smalldirkalex@gmail.com>
2022-11-18 22:38:38 +0800

    - update gitdiff.go

--- gitdiff.go
+++ gitdiff.go
@@ -1,10 +1,12 @@
 package gitdiff
 
 import (
+	"io/fs"
+	"io/ioutil"
 	"os"
 )
 
-func GetLeft() (string, error) {
+func GetLeftFile() (string, error) {
 	dat, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		return "", err
@@ -12,10 +14,18 @@ func GetLeft() (string, error) {
 	return string(dat), nil
 }
 
-func GetRight() (string, error) {
+func GetRightFile() (string, error) {
 	dat, err := os.ReadFile(os.Args[2])
 	if err != nil {
 		return "", err
 	}
 	return string(dat), nil
+}
+
+func GetLeftDirRecursive() ([]fs.FileInfo, error) {
+	return ioutil.ReadDir(os.Args[1])
+}
+
+func GetRightDirRecursive() ([]fs.FileInfo, error) {
+	return ioutil.ReadDir(os.Args[2])
 }
